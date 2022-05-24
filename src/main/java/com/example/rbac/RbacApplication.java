ea81101cd196ff8663cef8f11e055d4d9921ac6f
hezhongfeng <287645587@qq.com>
2022-05-24 10:28:50 +0800

    初始化项目

---
+++ src/main/java/com/example/rbac/RbacApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rbac/RbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
