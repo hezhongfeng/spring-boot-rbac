5a163e7d94d248b2a7b0a9cd8aa9b4cb28b91750
hezhongfeng <287645587@qq.com>
2022-05-25 15:09:43 +0800

    添加Controller

---
+++ src/main/java/com/example/rbac/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.rbac.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloworld")
+public class HelloWorldController {
+  @GetMapping("")
+  public String sayHello() {
+    return "Hello World!";
+  }
+}
