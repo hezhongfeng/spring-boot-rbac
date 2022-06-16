8329da7eb07f395cf234990e0a5ccbadea305cf0
hezhongfeng <287645587@qq.com>
2022-06-16 13:31:50 +0800

    去除生成密码的提示

--- src/main/java/com/example/rbac/RbacApplication.java
+++ src/main/java/com/example/rbac/RbacApplication.java
@@ -2,8 +2,9 @@ package com.example.rbac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class })
 public class RbacApplication {
 
 	public static void main(String[] args) {
